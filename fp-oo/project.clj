5437cb803652756c97ff5bb9d369100324036cb8
Nicolas Estrada <nicolas.estrada@alum.mit.edu>
2017-04-03 00:34:12 +0200

    Starting small exercises and getting a feel of Clojure workflow

---
+++ fp-oo/project.clj
@@ -0,0 +1,6 @@
+(defproject fp-oo "0.1.0"
+  :description "Functional Programming for the Object-Oriented Programmer exercises"
+  :url "https://github.com/neuromantik33/polyglot/"
+  :license {:name "MIT License"
+            :url  "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
