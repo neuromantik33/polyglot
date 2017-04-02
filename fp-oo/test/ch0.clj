907a25ac858db5f23670bb29f1fe3366cb5be8a5
ESTRADA Nicolas <nicolas.estrada@orange.com>
2017-04-03 00:31:48 +0200

    Some refactoring and cleaner code

--- fp-oo/test/ch0.clj
+++ fp-oo/test/ch0.clj
@@ -1,11 +1,9 @@
 (ns ch0
   (:require [clojure.test :refer :all]))
 
-;Define a function second that returns the second element of a list
+; Define a function second that returns the second element of a list
 (defn my-second [sequence]
-  (if (< (count sequence) 2)
-    nil
-    (nth sequence 1)))
+  (if (< (count sequence) 2) nil (nth sequence 1)))
 
 (deftest my-second-test
   (testing "(my-second '())    " (is (nil? (my-second '()))))
@@ -14,16 +12,31 @@
   (testing "(my-second [9 8 7])" (is (= (my-second [9 8 7]) 8)))
   )
 
+; Give 2 implementations of third
+(defn third-1 [sequence]
+  (if (< (count sequence) 3) nil (nth sequence 2)))
+(defn third-2 [sequence]
+  (second (rest sequence)))
+
+(deftest third-test
+  (testing "(third-1 [0 1 2 3 4])" (is (= (third-1 [0 1 2 3 4]) (third-2 [0 1 2 3 4]) 2)))
+  )
+
 ; Implement add-squares.
 (defn add-squares [& numbers]
-  (let [square (fn [x] (* x x))]
+  (let [square #(* % %)]
     (apply + (map square numbers))))
 
+; Book solution
+;(def add-squares
+;  (fn [& numbers]
+;    (apply + (map * numbers numbers))))
+
 (deftest add-squares-test
-  (testing "(add-squares)       " (is (= (add-squares) 0)))
-  (testing "(add-squares 1)     " (is (= (add-squares 1) 1)))
-  (testing "(add-squares 1 2)   " (is (= (add-squares 1 2) 5)))
-  (testing "(add-squares 1 2 3) " (is (= (add-squares 1 2 3) 14)))
+  (testing "(add-squares)      " (is (= (add-squares) 0)))
+  (testing "(add-squares 1)    " (is (= (add-squares 1) 1)))
+  (testing "(add-squares 1 2)  " (is (= (add-squares 1 2) 5)))
+  (testing "(add-squares 1 2 3)" (is (= (add-squares 1 2 3) 14)))
   )
 
 ; Using range and apply, implement a bizarre version of factorial
@@ -46,11 +59,16 @@
 (defn prefix-of? [candidate sequence]
   (let [c0 (first candidate)
         s0 (first sequence)]
-    (if (nil? c0)
-      true
-      (if (not= c0 s0)
-        false
-        (prefix-of? (rest candidate) (rest sequence))))))
+    (cond
+      (nil? c0) true
+      (not= c0 s0) false
+      :else (recur (rest candidate) (rest sequence)))))
+
+; Book solution
+;(def prefix-of?
+;  (fn [candidate seq]
+;    (= (take (count candidate) seq)
+;       candidate)))
 
 (deftest prefix-of-test
   (testing "(prefix-of [] [])            " (is (prefix-of? [] [])))
@@ -63,21 +81,27 @@
 ; Returns a sequence of successively smaller subsequences of the argument.
 
 ; Convoluted recursive solution!! (ie. mine)
-; (defn tails
-;   ([sequence]
-;    (if (empty? sequence) sequence (tails sequence '())))
-;   ([sequence result]
+;(defn tails [sequence]
+;  (loop [seq sequence
+;         result '()]
 ;    (let [n (count result)]
-;      (if (= (count sequence) n)
-;        (reverse (conj result '()))
-;        (let [item (drop n sequence)]
-;          (tails sequence (conj result item)))))))
+;      (if (< (count seq) n)
+;        (reverse result)
+;        (let [item (drop n seq)]
+;          (recur seq (conj result item)))))))
 
 ; Much more elegant (also mine :))
 (defn tails [sequence]
   (let [len (count sequence)
-        drop-seq (fn [i] (drop i sequence))]
+        drop-seq #(drop % sequence)]
     (map drop-seq (range (inc len)))))
+
+; Book solution
+;(def tails
+;  (fn [seq]
+;    (map drop
+;         (range (inc (count seq)))
+;         (repeat (inc (count seq)) seq))))
 
 (deftest tails-test
   (testing "(tails '())       " (is (= (tails '()) '(()))))
